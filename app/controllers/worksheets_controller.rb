142fdc44f84c44937263b12ece0d79e2f2bcadc4
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-08-01 16:07:58 -0500

    Skip the select worksheet page if there is just one worksheet

--- app/controllers/worksheets_controller.rb
+++ app/controllers/worksheets_controller.rb
@@ -1,5 +1,10 @@
 class WorksheetsController < ApplicationController
   def index
-    @spreadsheet = Cocupu::Spreadsheet.find(params[:spreadsheet_id])
+    spreadsheet = Cocupu::Spreadsheet.find(params[:spreadsheet_id])
+    @worksheets = spreadsheet.worksheets
+    if @worksheets.size == 1
+      redirect_to new_mapping_template_path(:mapping_template=>{:worksheet_id=>@worksheets.first.id})
+      return
+    end
   end
 end
--- spec/controllers/worksheets_controller_spec.rb
+++ spec/controllers/worksheets_controller_spec.rb
@@ -1,14 +1,26 @@
 require 'spec_helper'
 
 describe WorksheetsController do
-  describe 'index' do
+  describe 'index with multiple worksheets' do
     before do
-      @spreadsheet = Cocupu::Spreadsheet.create!()
+      @worksheet1 = Worksheet.new
+      @worksheet2 = Worksheet.new
+      @spreadsheet = Cocupu::Spreadsheet.create!(:worksheets=>[@worksheet1, @worksheet2])
     end
     it "should be success" do
       get :index, :spreadsheet_id => @spreadsheet.id
-      assigns[:spreadsheet].should == @spreadsheet
+      assigns[:worksheets].should include(@worksheet1, @worksheet2)
       response.should be_success
+    end
+  end
+  describe 'index with a single worksheet' do
+    before do
+      @worksheet = Worksheet.new
+      @spreadsheet = Cocupu::Spreadsheet.create!(:worksheets=>[@worksheet])
+    end
+    it "should be success" do
+      get :index, :spreadsheet_id => @spreadsheet.id
+      response.should redirect_to new_mapping_template_path(:mapping_template=>{:worksheet_id=>@worksheet.id}) 
     end
   end
 
