01bdaa1b57cce19f5ebdd7bf714a7948f958f985
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-09-27 13:42:05 -0500

    Fixed test for application helper

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -14,8 +14,8 @@ describe ApplicationHelper do
   end
 
   it "should render_facet_list" do
-    out = helper.render_facet_list('name_s', ['Aluminium', '7'])
-    out.should == "<ul><li><a href=\"/#{@exhibit.pool.owner.short_name}/#{@exhibit.pool.short_name}/exhibits/#{@exhibit.id}?f%5Bname_s%5D=Aluminium&amp;q=term\" title=\"Aluminium\">Aluminium (7)</a></li></ul>"
+    out = helper.render_facet_list('name_s', ['Aluminium', '7'], 'myid')
+    out.should == "<ul class=\"accordion_body collapse\" id=\"myid\"><li><a href=\"/#{@exhibit.pool.owner.short_name}/#{@exhibit.pool.short_name}/exhibits/#{@exhibit.id}?f%5Bname_s%5D=Aluminium&amp;q=term\" title=\"Aluminium\">Aluminium (7)</a></li></ul>"
     
   end
 
