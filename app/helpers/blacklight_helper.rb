08cfec8984908f7b5aab950ef85b39e627ee6669
Ubuntu <ubuntu@ip-10-122-42-138.ec2.internal>
2012-11-01 21:38:38 +0000

    A more concise way to fix the links to the documents

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -1,18 +1,7 @@
 module BlacklightHelper
   include Blacklight::BlacklightHelperBehavior
-
-  def link_to_previous_document(previous_document)
-    path = identity_exhibit_solr_document_path(@identity, @exhibit, previous_document) if previous_document
-    link_to_unless previous_document.nil?, raw(t('views.pagination.previous')), path, :class => "previous", :rel => 'prev', :'data-counter' => session[:search][:counter].to_i - 1 do
-      content_tag :span, raw(t('views.pagination.previous')), :class => 'previous'
-    end
+  def solr_document_path(doc)
+        identity_exhibit_solr_document_path(@identity, @exhibit, doc) if doc
   end
-
-  def link_to_next_document(next_document)
-    path = identity_exhibit_solr_document_path(@identity, @exhibit, next_document) if next_document
-    link_to_unless next_document.nil?, raw(t('views.pagination.next')), path, :class => "next", :rel => 'next', :'data-counter' => session[:search][:counter].to_i + 1 do
-      content_tag :span, raw(t('views.pagination.next')), :class => 'next'
-    end
-  end 
 
 end
