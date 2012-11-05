9e757cdbaa6bc29a4faa59f8a6941ea62f997fb7
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-11-05 13:36:44 -0600

    Added specs for tokens controller

--- spec/controllers/api/v1/tokens_controller_spec.rb
+++ spec/controllers/api/v1/tokens_controller_spec.rb
@@ -2,4 +2,20 @@ require 'spec_helper'
 
 describe Api::V1::TokensController do
 
+  before do
+    @identity = FactoryGirl.create :identity
+  end
+
+  it "should return an error" do
+    post :create, :format=>:json
+    response.code.should == '400'
+    JSON.parse(response.body)['message'].should == 'The request must contain the user email and password.'
+  end
+  it "should return a token" do
+    LoginCredential.any_instance.stub(:authentication_token=>'9t1pLZEAKQ819ZpBQ3uK')
+    post :create, :email=>@identity.login_credential.email, :password=>@identity.login_credential.password, :format=>:json
+    response.should be_successful
+    JSON.parse(response.body)['token'].should == '9t1pLZEAKQ819ZpBQ3uK'
+  end
+
 end
