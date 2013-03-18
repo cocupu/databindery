05de4f26886c576ccd3fc5adb1ddb0a00e63c366
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2013-03-18 14:33:53 +0545

    [s3] slightly improved handling of hostname for setting cors on buckets for upload

--- app/models/s3_connection.rb
+++ app/models/s3_connection.rb
@@ -19,7 +19,7 @@ class S3Connection < ActiveRecord::Base
     bucket = ensure_bucket_initialized(bucket_name)
     allowed_origins = bucket.cors.select{|c| c.allowed_methods.include?("POST")}.map{|c| c.allowed_origins}.flatten
     unless allowed_origins.include?("*") || allowed_origins.include?(Socket.gethostname)
-      rule = AWS::S3::CORSRule.new(allowed_methods:["PUT","POST", "GET"], allowed_origins:[Socket.gethostname], allowed_headers:["*"])
+      rule = AWS::S3::CORSRule.new(allowed_methods:["PUT","POST", "GET"], allowed_origins:[hostname], allowed_headers:["*"])
       bucket.cors.set(rule)
     end
   end
@@ -37,5 +37,13 @@ class S3Connection < ActiveRecord::Base
   def conn
     @conn ||= AWS::S3.new(:access_key_id => access_key_id,
                           :secret_access_key => secret_access_key)
+  end
+  
+  def hostname
+    name = Socket.gethostname
+    if Rails.env == "production"
+      name = "http://"+name unless name.include?("http://")
+    end
+    name
   end
 end
