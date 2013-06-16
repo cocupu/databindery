827b88ed8c4053ff492799ff324191e2e455b92c
Matt Zumwalt <matt@curationexperts.com>
2013-06-16 17:30:39 -0500

    SENSITIVE DATA setting up action_mailer smtp through gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,16 @@ Bindery::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'cocupu.com',
+    user_name:            'systems@cocupu.com',
+    password:             'whci>msb74',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 
   # Enable threaded mode
   # config.threadsafe!
