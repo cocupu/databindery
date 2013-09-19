64f16dc3be5129c12e63262e254098a5e80fa5e6
Matt Zumwalt <matt@curationexperts.com>
2013-09-19 12:50:24 -0500

    Switch to database session store

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130804200354) do
+ActiveRecord::Schema.define(:version => 20130903035509) do
 
   create_table "access_controls", :force => true do |t|
     t.integer  "pool_id"
@@ -182,9 +182,10 @@ ActiveRecord::Schema.define(:version => 20130804200354) do
     t.string   "field_name"
     t.string   "operator"
     t.text     "values"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
     t.integer  "exhibit_id"
+    t.string   "association_code"
   end
 
   create_table "searches", :force => true do |t|
@@ -196,6 +197,16 @@ ActiveRecord::Schema.define(:version => 20130804200354) do
   end
 
   add_index "searches", ["user_id"], :name => "index_searches_on_user_id"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "spreadsheet_rows", :force => true do |t|
     t.integer  "row_number"
